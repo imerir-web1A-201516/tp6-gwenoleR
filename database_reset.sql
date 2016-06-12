0da3896fd122c3e86a868eb5a34460c6c55a08b7
gwenoleR <gwenole.roton@live.com>
2016-06-12 17:29:29 +0200

    fin tp6

--- database_reset.sql
+++ database_reset.sql
@@ -1,3 +1,6 @@
 DROP TABLE IF EXISTS prets;
-CREATE TABLE prets (quoi varchar);
-INSERT INTO prets (quoi) VALUES ('test');
+CREATE TABLE prets (id SERIAL PRIMARY KEY ,
+                    qui varchar,
+                    quoi varchar,
+                    statut varchar);
+INSERT INTO prets (qui, quoi, statut) VALUES ('fvargas','NXT', 'prete');
--- main.py
+++ main.py
@@ -21,13 +21,75 @@ def route_dbinit():
 @app.route('/prets', methods=['GET'])
 def prets_fetchall():
   db = Db()
-  result = db.select("SELECT * FROM prets")
+  result = db.select("SELECT * FROM prets WHERE statut='prete'")
   db.close()
   
   resp = make_response(json.dumps(result))
   resp.mimetype = 'application/json'
   return resp
 
+#-----------------------------------------------------------------
+
+@app.route('/prets', methods=['POST'])
+def pret_add():
+  try :
+    data = request.get_json()
+    db = Db()
+    result = db.select("INSERT INTO prets (qui, quoi, statut) VALUES (%(qui)s,%(quoi)s, %(statut)s) RETURNING id",{
+      'qui' : data['qui'],
+      'quoi' : data['quoi'],
+      'statut' : data['statut']
+    })
+    
+    
+    resp = make_response('', 201)
+    resp.mimetype = 'application/json'
+    resp.headers['Location'] = "/prets/%d" % result[0]['id']
+    db.close()
+    return resp
+  except:
+    resp = make_response('', 400)
+    return resp
+
+#-----------------------------------------------------------------
+
+@app.route('/prets/<int:id>', methods=['GET'])
+def prets_fetchone(id):
+  db = Db()
+  result = db.select("SELECT * FROM prets WHERE id=%(id)s",{
+    'id' : id
+  })
+  db.close()
+    
+  if len(result) != 1:
+    return make_response("Not found", 404)
+    
+  resp = make_response(json.dumps(result),200)
+  resp.mimetype = 'application/json'
+  return resp
+  
+#-----------------------------------------------------------------
+
+@app.route('/prets/<int:id>', methods=['PUT'])
+def prets_changeone(id):
+  try :
+    data = request.get_json()
+    db = Db()
+    result = db.execute("UPDATE prets SET qui = %(qui)s, quoi = %(quoi)s, statut = %(statut)s WHERE id=%(id)s",{
+      'qui' : data['qui'],
+      'quoi' : data['quoi'],
+      'statut' : data['statut'],
+      'id' : id
+    })
+    
+    
+    resp = make_response('', 204)
+    db.close()
+    return resp
+  except:
+    resp = make_response('', 400)
+    return resp
+  
 #-----------------------------------------------------------------
 
 if __name__ == "__main__":
